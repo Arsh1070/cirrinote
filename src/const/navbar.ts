ebacec1f0ae30abde80dd1bdcb80960308a55629
Arsh Brar <arshbrar@Arshs-MacBook-Air.local>
2026-01-07 11:52:48 +0530

    add

--- src/const/navbar.ts
+++ src/const/navbar.ts
@@ -3,7 +3,6 @@ export const navItems = [
   { path: "#features", name: "Features" },
   { path: "#contact", name: "FAQs" },
   { path: "#contact", name: "Contact" },
-  sd,
   /* { path: "#help", name: "Help" },
   { path: "#mobile", name: "Mobile" }, */
 ];
