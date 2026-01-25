90caafdf7c6c1e5986ebfd26801b03d305c4169b
Arsh Brar <arshbrar@Arshs-MacBook-Air.local>
2026-01-25 12:37:14 +0530

    initial commit

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -5,8 +5,8 @@ import NotesSection from "@/components/home/notesSection";
 import "@/styles/layout.css";
 
 export default function Home() {
-  testFn();
-  const testFn = () => {};
+  testFnCall();
+  const testFnCall = () => {};
   return (
     <>
       <HeroSection />
